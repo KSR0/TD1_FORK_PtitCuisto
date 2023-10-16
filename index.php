1c725c332491edd8385036f318f711b308366c61
KSR0 <nicolas.peyregne@gmail.com>
2023-10-16 19:29:05 +0200

    Minor changes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-    require_once 'includes/connexionBDD.php';
-
     require_once 'templates/header.php';
+
+    require_once 'includes/connexionBDD.php';
 
     require_once 'templates/footer.php';
 ?>
