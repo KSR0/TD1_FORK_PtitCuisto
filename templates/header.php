403512a0ceda9f8e7ad1872e9bc8cd8caaf99015
KSR0 <nicolas.peyregne@gmail.com>
2023-10-17 20:50:53 +0200

    Minor changes

--- templates/header.php
+++ templates/header.php
@@ -22,10 +22,10 @@
     </head>
 
     <body class="font-pacifico text-2xl m-1">
-        <img class="object-scale-down h-32 w-64 m-5" src="img/Logo.png" alt="Logo du site" title="Logo P'tit Cuito !">
+        <img class="object-scale-down w-64 m-5" src="img/Logo.png" alt="Logo du site" title="Logo P'tit Cuito !">
         <h1 class="text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5">PHP - Site du P'tit Cuisto !</h1>
         <p class="text-center text-charte_bleu_clair">Ceci est un test</p>
-        <img class="mx-auto object-scale-down h-72 w-144 mt-5" src="img/Edito.png" alt="Image de présentation de l’édito" title="Edito !">
+        <img class="mx-auto object-scale-down h-72 w-144 mt-5" src="img/Edito.png" alt="Image de présentation de l'édito" title="Edito !">
 
         <?php
             // Appel de la fonction de la requête SQL permettant d'afficher les utilisateurs
