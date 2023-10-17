6399ec9da4764e239e545c3e889de1743cc00eae
KSR0 <nicolas.peyregne@gmail.com>
2023-10-17 17:25:13 +0200

    Creating request for testing

--- index.php
+++ index.php
@@ -3,5 +3,14 @@
 
     require_once 'includes/connexionBDD.php';
 
+    $requete = "SELECT * FROM FORK_UTILISATEUR";
+    
+    $reqServeur = $bdd->prepare($requete);
+    $reqServeur->execute();
+    $utilisateurs = $reqServeur->fetchAll();
+    foreach($utilisateurs as $utilisateur) {
+        echo "ID : " . $utilisateur["USER_ID"] . "<PRE></br></PRE>" . "Pseudo : " . $utilisateur["USER_PSEUDO"] . "<PRE></br></PRE>" . "Email : " . $utilisateur["USER_EMAIL"] . "<PRE></br></PRE>" . "Date inscription : " . $utilisateur["USER_DATE_INS"] . "<PRE></br></PRE>";
+    }
+
     require_once 'templates/footer.php';
 ?>
--- templates/header.php
+++ templates/header.php
@@ -21,5 +21,3 @@
         <h1 class="text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5">PHP - Site du P'tit Cuisto !</h1>
         <p class="text-center text-charte_bleu_clair">Ceci est un test</p>
         <img class="mx-auto object-scale-down h-72 w-144 mt-5" src="img/Edito.png" alt="Image de présentation de l’édito" title="Edito !">
-    </body>
-</html>
