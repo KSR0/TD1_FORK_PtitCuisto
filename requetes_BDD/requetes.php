5bf7b29220fbdb61c847c8285d3e1aea28dafe3a
Bayon Axel <axel.bayon14@gmail.com>
2023-10-17 20:53:36 +0200

    Updated the ideal syntax to follow when writing a function containing an SQL query

--- requetes_BDD/requetes.php
+++ requetes_BDD/requetes.php
@@ -2,16 +2,17 @@
     require_once 'includes/connexionBDD.php';
 
     function afficherUtilisateurs($bdd) {
-        $requete = "SELECT * FROM FORK_UTILISATEUR";
+        $requete = "SELECT * FROM FORK_TYPE";
         $reqServeur = $bdd->prepare($requete);
         $reqServeur->execute();
-        $utilisateurs = $reqServeur->fetchAll();
+        $types = $reqServeur->fetchAll();
     
-        foreach($utilisateurs as $utilisateur) {
-            echo "ID : " . $utilisateur["USER_ID"] . "<br>" . "Pseudo : "
-            . $utilisateur["USER_PSEUDO"] . "<br>" . "Email : "
-            . $utilisateur["USER_EMAIL"] . "<br>" . "Date inscription : "
-            . $utilisateur["USER_DATE_INS"] . "<br>";
+        echo '<p class="text-charte_bleu_clair">Types de comptes existants :</p>';
+
+        foreach($types as $types_comptes) {
+            echo "<br>
+            ID : ". $types_comptes["TYP_ID"] . "<br>" . 
+            "Type de compte : ". $types_comptes["TYP_INTITULE"] . "<br>";
         }
     }
     
--- templates/header.php
+++ templates/header.php
@@ -24,7 +24,7 @@
     <body class="font-pacifico text-2xl m-1">
         <img class="object-scale-down h-32 w-64 m-5" src="img/Logo.png" alt="Logo du site" title="Logo P'tit Cuito !">
         <h1 class="text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5">PHP - Site du P'tit Cuisto !</h1>
-        <p class="text-center text-charte_bleu_clair">Ceci est un test</p>
+        <p class="text-4xl text-center text-charte_bleu_clair">Ceci est un test</p>
         <img class="mx-auto object-scale-down h-72 w-144 mt-5" src="img/Edito.png" alt="Image de présentation de l’édito" title="Edito !">
 
         <?php
