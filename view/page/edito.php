b222f44c76329336b72ee70f81e454c6df333298
KSR0 <nicolas.peyregne@gmail.com>
2023-10-20 15:35:46 +0200

    Changing Acceuil to Accueil

--- view/page/edito.php
+++ view/page/edito.php
@@ -6,7 +6,7 @@
 <script src="../../controller/page/script_edito.js"></script>
 
 <h1 class="text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5">Site du P'tit Cuisto !</h1>
-<p class="text-3xl text-center text-charte_bleu_clair">Édito - Acceuil.</p>
+<p class="text-3xl text-center text-charte_bleu_clair">Édito - Accueil.</p>
 <img class="mx-auto object-scale-down h-72 w-144 mt-5" src="../../img/Edito.png" alt="Image de présentation de l'édito" title="Edito !">
 
 <?php
