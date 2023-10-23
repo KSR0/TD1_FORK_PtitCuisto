dd754ad8426b9122a614f3bd479bea04e2e1aca8
Bayon Axel <axel.bayon14@gmail.com>
2023-10-23 11:16:05 +0200

    minor change

--- view/template.php
+++ view/template.php
@@ -15,6 +15,10 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
 
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+
     </head>
 
     <body class="font-pacifico text-2xl m-1">
