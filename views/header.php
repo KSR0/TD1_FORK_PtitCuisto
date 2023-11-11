2e2dc587917f98d75617fd2de1ae9886972fa1e2
Leny <leny.meziere@etu.unicaen.fr>
2023-11-11 15:09:17 +0100

    log off fix

--- views/header.php
+++ views/header.php
@@ -109,12 +109,15 @@
                 </div>
                 </a>
 
-                <a href="index.php?action=edito&deconnexion=true">
-                <div class="element_menu cursor-pointer p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 border-2 border-charte_blanc bg-charte_bleu_clair hover:bg-charte_bleu_fonce">
-                    <i class="bi bi-box-arrow-in-right"></i>
-                    <p class="modal-open2 text-[15px] ml-4 text-gray-200">Se deconnecter</p>
-                </div>
-                </a>
+                <?php if(isset($_SESSION['user_pseudo'])) {
+                    echo "<a href='index.php?action=edito&deconnexion=true'>
+                    <div class='element_menu cursor-pointer p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 border-2 border-charte_blanc bg-charte_bleu_clair hover:bg-charte_bleu_fonce'>
+                        <i class='bi bi-box-arrow-in-right'></i>
+                        <p class='modal-open2 text-[15px] ml-4 text-gray-200'>Se deconnecter</p>
+                    </div>
+                    </a>";
+                }
+                ?>
 
                 <hr class="my-4 text-gray-600">
 
