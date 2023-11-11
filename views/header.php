bb809974cdd04c863ca846af77b166638f9b8018
Bayon Axel <axel.bayon14@gmail.com>
2023-11-11 14:43:28 +0100

    Re-adding temporary buttons

--- views/header.php
+++ views/header.php
@@ -121,3 +121,28 @@
     }
     dropDown()
 </script>
+
+<!-- Permet de passer d'une page à une autre facilement pour faire des tests sans passer par le menu -->
+<div class="text-center lg:pl-80 lg:pt-5 min-[320px]:pt-20 md:pt-20 pr-5 pt-5">
+
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    onclick="bouton_detail_recette()">Détails de la recette</button>
+
+    <a href="index.php?action=creation_recette">
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    >Création d'une recette</button>
+    </a>
+
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    onclick="bouton_modification_recette()">Modifier ma recette</button>
+
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    onclick="bouton_creation_compte()">Création d'un compte</button>
+
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    onclick="bouton_mon_compte()">Mon compte</button>
+
+    <button class="ml-5 text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" 
+    onclick="bouton_modification_compte()">Modifier mon compte</button>
+
+</div>
