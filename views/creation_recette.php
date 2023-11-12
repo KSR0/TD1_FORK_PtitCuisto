8853444003911efc38352aad14d322811d73ae8f
Bayon Axel <axel.bayon14@gmail.com>
2023-11-12 19:04:06 +0100

    Style for creating a recipe

--- views/creation_recette.php
+++ views/creation_recette.php
@@ -1,58 +1,75 @@
-<?php ob_start();?>
-
-
+<?php ob_start(); ?>
 
 <!-- ↓----------------------------------------------------↓ Code de la page ↓----------------------------------------------------↓ -->
 
-<h1 class="text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5">Crée une recette</h1>
-<p class="text-3xl text-center text-charte_bleu_clair">Remplissez le formulaire ci-dessous pour crée votre propre recette.</p><br>
+<h1 class='text-center text-charte_bleu_fonce font-permanent_marker text-5xl mb-5'>Création d'une recette</h1>
 
 <form action="index.php?action=requete_creation_recette" method="post">
+    <div id="recette">
+        <div class='border-2 border-charte_bleu_clair h-fit rounded-lg pt-2 px-4 mb-2 mr-2'>
+            <div class='flex'>
 
-   <div>
-  	<label for="titre">Titre de la recette : </label>
-  	<input type="text" id="titre" name="titre" placeholder="Titre" />
-   </div>
+                <div id='tag_image' class='w-1/2 max-h-div_recette text-center p-4 mr-2'>
+                    <div class='text-charte_bleu_fonce border-2 border-charte_bleu_clair rounded-lg p-2 mb-2'>
+                        <label class='font-permanent_marker text-3xl' for='lien_image'>Entrez le lien de l'image : </label><br>
+                        <div class="my-2">
+                            <textarea class="text-charte_bleu_clair border border-charte_bleu_fonce rounded-lg p-2 w-full" type="text" id="lien_image" name="lien_image" placeholder="Exemple : https://le_lien_de_l_image_de_la_recette"></textarea><br>
+                        </div>
+                    </div>
 
-   <div>
-        <label for="categorie">Categorie de la recette : </label>
-        <select name="categorie" id="categorie-select">
-            <option value="">-- Choisir une categorie de plat --</option>
-            <option value="entree">Entrée</option>
-            <option value="plat">Plat</option>
-            <option value="dessert">Dessert</option>
-        </select>
+                    <div class='text-center text-charte_blanc border-2 border-charte_bleu_clair rounded-lg bg-charte_bleu_clair p-2 mb-2'>
+                        <label class='font-permanent_marker text-3xl' for="tags">Entrez les tags (separés par une virgule) : </label>
+                        <div class="my-2">
+                            <textarea class="text-charte_bleu_clair border border-charte_bleu_fonce rounded-lg p-2 w-full" type="text" id="tags" name="tags" placeholder="Exemple : #Noel, #Hiver"></textarea>
+                        </div>
+                    </div>
+                </div>
+
+                <div id='div_infos' class='text-charte_bleu_fonce border-l-2 border-charte_bleu_fonce w-1/2 py-2 px-4'>
+                    <label class="font-permanent_marker text-3xl" for="titre">Entrez le titre de votre recette: </label><br>
+                    <div class="my-2">
+                        <input class="text-charte_bleu_clair border border-charte_bleu_fonce rounded-lg p-2 w-full" type="text" id="titre" name="titre" placeholder="Exemple : Tarte aux pommes"/><br><br>
+                    </div>
+
+                    <label class="font-permanent_marker text-3xl" for="categorie">Sélectionnez la catégorie de la recette : </label>
+                    <div class="my-2">
+                        <select class="text-charte_bleu_clair cursor-pointer border border-charte_bleu_fonce rounded-lg p-2 " name="categorie" id="categorie-select">
+                            <option class="text-charte_gris" value="">-- Choisissez une catégorie --</option>
+                            <option class="text-charte_bleu_clair" value="entree">Entrée</option>
+                            <option class="text-charte_bleu_clair" value="plat">Plat</option>
+                            <option class="text-charte_bleu_clair" value="dessert">Dessert</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <hr class='border-2 border-charte_bleu_fonce my-4 mx-auto'>
+
+            <div class='flex'>
+                <div id='resume' class='w-1/2 text-charte_bleu_fonce py-2 px-4'>
+                    <label class="font-permanent_marker text-3xl" for="resume">Entrez le résumé de la recette : </label><br>
+                    <div class="my-2">
+                        <textarea class="text-charte_bleu_clair border border-charte_bleu_fonce rounded-lg p-2 w-full" id="resume" name="resume" placeholder="Exemple : Cette version de la tarte aux pommes est une spécialité normande..."></textarea>
+                    </div>
+                </div>
+
+                <div class='w-1/2 border-l-2 border-charte_bleu_fonce mb-2'>
+                    <div id='contenu' class='text-charte_bleu_fonce pt-2 px-4'> 
+                        <label class="font-permanent_marker text-3xl" for="contenu">Entrez le contenu de la recette : </label><br>
+                        <div class="my-2">
+                            <textarea class="text-charte_bleu_clair border border-charte_bleu_fonce rounded-lg p-2 w-full" id="contenu" name="contenu" placeholder="Exemple : Matériel : moule, couteau, four..."></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 
-   <div>
-  	<label for="contenu">Contenu de la recette : </label>
-  	<textarea id="contenu" name="contenu" placeholder="Contenu"></textarea>
-   </div>
-
-   <div>
-  	<label for="resume">Resume de la recette : </label>
-  	<input type="text" id="resume" name="resume" placeholder="Resume"></input>
-   </div>
-
-   <div>
-  	<label for="tags">Tags de la recette (separés par une virgule) : </label>
-  	<input class="w-3/5" type="text" id="tags" name="tags" placeholder="Ex : Noel, Hiver"></input>
-   </div>
-
-
-
-   <div>
-  	<label for="lien_image">Lien de l'image de la recette : </label>
-  	<input type="text" id="lien_image" name="lien_image" placeholder="https://..."></input>
-   </div>
-
-   <br>
-   <div>
-  	<input type="submit" class="text-charte_bleu_clair hover:text-charte_bleu_fonce border border-charte_bleu_fonce hover:bg-charte_bleu_clair focus:ring-4 focus:outline-none font-medium rounded-lg text-lg px-5 py-2.5 text-center mr-2 mb-2"/>
-   </div>
+    <button class="font-permanent_marker cursor-pointer p-2.5 mt-3 flex justify-center rounded-md px-4 duration-300 border-2 border-charte_bleu_fonce text-charte_blanc mx-auto bg-charte_bleu_clair hover:border-charte_bleu_clair hover:bg-charte_bleu_fonce">
+        <p type="submit">Créer une recette</p>
+    </button>
 </form>
 
 <?php $content = ob_get_clean();
 require_once('template.php');
 ?>
-
