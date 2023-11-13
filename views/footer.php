b8dce8119f37530155c4351a8dbb5f4878c868c3
Bayon Axel <axel.bayon14@gmail.com>
2023-11-13 21:18:27 +0100

    Final footer

--- views/footer.php
+++ views/footer.php
@@ -1,6 +1,6 @@
 
 
-<div class="w-full bg-charte_bleu_fonce max-w-screen-xl mx-auto px-2 pt-4 md:py-8">
+<div class="w-full bg-charte_bleu_fonce max-w-screen-xl mx-auto py-2 mt-4">
     <p class="text-charte_blanc block text-xl font-permanent_marker text-center">© 2023 
         <a href="index.php" class="hover:underline hover:text-charte_bleu_clair" title="Aller à l'accueil">P'tit Cuisto™</a>. 
         Axel Bayon - Leny Meziere - Nicolas Peyregne
