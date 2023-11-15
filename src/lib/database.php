2d2765ffcb300cf0aa8a4333dd4e820bee6dc3b8
KSR0 <nicolas.peyregne@gmail.com>
2023-11-15 23:04:36 +0100

    Minor change

--- src/lib/database.php
+++ src/lib/database.php
@@ -5,11 +5,11 @@ class DatabaseConnection
 	public ?PDO $database = null;
 
 	public function getConnection(): PDO {
-        $db_host = parse_ini_file('all_secret_variables.env')["DB_HOST"];
-        $db_name = parse_ini_file('all_secret_variables.env')["DB_NAME"];
-        $db_encode = parse_ini_file('all_secret_variables.env')["DB_ENCODE"];
-        $db_username = parse_ini_file('secret_variables.env')["DB_USERNAME"];
-        $db_password = parse_ini_file('secret_variables.env')["DB_PASSWORD"];
+        $db_host = parse_ini_file('bayon222_all_secret_variables.env')["DB_HOST"];
+        $db_name = parse_ini_file('bayon222_all_secret_variables.env')["DB_NAME"];
+        $db_encode = parse_ini_file('bayon222_all_secret_variables.env')["DB_ENCODE"];
+        $db_username = parse_ini_file('bayon222_all_secret_variables.env')["DB_USERNAME"];
+        $db_password = parse_ini_file('bayon222_all_secret_variables.env')["DB_PASSWORD"];
         
     	if ($this->database === null) {
         	$this->database = new PDO('mysql:host=' . $db_host . ';dbname=' . $db_name . ';charset=' . $db_encode, $db_username, $db_password);
